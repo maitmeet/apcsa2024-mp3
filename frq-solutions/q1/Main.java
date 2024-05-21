e7ec995239ad2076e54b7edec219d5e48d1318ee
maitmeet <alinam84@nycstudents.net>
2024-05-21 11:51:58 -0400

    Update Main.java

--- frq-solutions/q1/Main.java
+++ frq-solutions/q1/Main.java
@@ -1,1 +1,22 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Question 1 FRQ");
 
+    Feeder f1 = new Feeder(2400);
+
+    System.out.println(f1.simulateManyDays(10, 4));
+    int food = f1.getFood();
+    System.out.println(food);
+
+    Feeder f2 = new Feeder(0);
+
+    System.out.println(f2.simulateManyDays(10, 4));
+    int food2 = f2.getFood();
+    System.out.println(food2);
+
+    Feeder f3 = new Feeder(200);
+    f3.simulateOneDay(5);
+    System.out.println(f3.getFood());
+  }
+  
+}
